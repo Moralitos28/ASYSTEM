e19fe9d760aaae03e4857d68ba696eea913ae90d
Moralitos28 <carlos-morales28@hotmail.es>
2020-05-28 12:43:23 -0600

    some changes

---
+++ ControlTower_Beta/config.h
@@ -0,0 +1,7 @@
+const char* ssid     = "FamiliaMorales";
+const char* password = "casa240409";
+const char* hostname = "ASYSTEM_BETA";
+
+IPAddress ip(192, 168, 1, 95);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
